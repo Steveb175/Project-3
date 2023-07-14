77306cfb37174b31fa9c2f4e4a1ffd909aa63f59
kshwi <kshwiss@gmail.com>
2023-07-13 19:02:43 -0500

    adding the model schema and seeds

---
+++ server/models/Workouts.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const workoutSchema = new Schema({
+  name: {
+    type: String
+  },
+  instructions: {
+    type: String
+  },
+  muscle: {
+    type: String
+  }
+});
+
+const Workouts = model("Workouts", workoutSchema);
+
+module.exports = Workouts;
