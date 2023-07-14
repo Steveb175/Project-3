5020705e35e567a758f95d069a1b26057d63de1b
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-07-13 20:31:06 -0500

    updates to pages, headers, app.js communication

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 // GraphQL API endpoint
 const httpLink = createHttpLink({ uri: "/graphql" });
