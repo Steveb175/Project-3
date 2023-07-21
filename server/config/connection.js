ae7cb5c847e3fe6e3b75c394c59018b23af5c0f8
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-07-20 21:21:15 -0500

    Heroku changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/workout-stuff"
+);
 
 module.exports = mongoose.connection;
